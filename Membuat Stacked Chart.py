3e4fdde019429be5ba1916f96abc4f5fb1f031c2
Xoxxox21 <fauzan01062000@gmail.com>
2022-11-08 15:01:41 +0700

    menambahkan ncol pada legend

--- Membuat Stacked Chart.py
+++ Membuat Stacked Chart.py
@@ -7,10 +7,10 @@ dataset['gmv'] = dataset['item_price']*dataset['quantity']
 dataset_dki_q4 = dataset[(dataset['province']=='DKI Jakarta') & (dataset['order_month'] >= '2019-10')]
 
 dataset_dki_q4.groupby(['order_month','city'])['gmv'].sum().sort_values(ascending=False).unstack().plot(kind='bar',stacked=True)
-plt.title('GMV Per Month, Breakdown by Month\nDKI Jakarta in Q4 2019',loc='center',pad=30,fontsize=15,color='blue')
+plt.title('GMV Per Month, Breakdown by City\nDKI Jakarta in Q4 2019',loc='center',pad=30,fontsize=15,color='blue')
 plt.xlabel('Order Month', fontsize=12)
 plt.ylabel('Total Amount (in Billions)', fontsize=12)
-plt.legend(bbox_to_anchor=(1, 1), shadow=True, title='City')
+plt.legend(bbox_to_anchor=(1, 1), shadow=True, ncol=1, title='City')
 plt.ylim(ymin=0)
 labels, locations = plt.yticks()
 plt.yticks(labels, (labels/1000000000).astype(int))
